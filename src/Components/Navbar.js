84d2f1002b8f53e305f2a907270d98116855f35b
abbycavallari <abigailkehinde58@gmail.com>
2021-08-09 15:26:23 +0100

    new

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+        <nav className="navbar">
+            <div>
+
+            </div>
+        </nav>
+    
+        </>
+    )
+}
+
+export default Navbar
